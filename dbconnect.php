47d40ff4daaef3b79f661a217f237dbe252b15b7
qinxiong <tanq0046@e.ntu.edu.sg>
2024-05-30 11:49:26 +0800

    commit

--- dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	@ $db = new mysqli('localhost', 'root', '', 'dg03_project');
-	if (mysqli_connect_errno()) {
-     echo 'Error: Could not connect to the database. Please try again later.';
-     exit;
-	 
-	}
-?>
